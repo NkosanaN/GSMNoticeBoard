ad6addf311fef9c5dcd1598733e03a95f2110f36
Nkosana Ndlela <nkosana.prince.ndlela@gmail.com>
2020-05-03 12:37:44 +0200

    Add project files.

---
+++ GSMElectronicNB/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using GSMElectronicNB.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSMElectronicNB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SmsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GSMElectronicNB/View/SmsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSMElectronicNB.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SmsPage : ContentPage
+    {
+        public SmsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSendSms(object sender, EventArgs e)
+        {
+            try
+            {
+                await Sms.ComposeAsync(new SmsMessage(EntryMessage.Text, EntryNumber.Text)); 
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
