a0e88e918fcbc49d13d5dd2fc297f9e96e832fb1
Nkosana Ndlela <nkosana.prince.ndlela@gmail.com>
2020-05-31 11:12:39 +0200

    Image Render

--- GSMElectronicNB.Android/MainActivity.cs
+++ GSMElectronicNB.Android/MainActivity.cs
@@ -21,8 +21,9 @@ namespace GSMElectronicNB.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             //XF.Material.Forms.Material.Init(this);
-            //XF.Material.Droid.Material.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            XF.Material.Droid.Material.Init(this, savedInstanceState);
+           
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- GSMElectronicNB/View/sLoginPage.xaml.cs
+++ GSMElectronicNB/View/sLoginPage.xaml.cs
@@ -21,71 +21,71 @@ namespace GSMElectronicNB.View
             InitializeComponent();
         }
 
-        bool checkInputs() 
-        {
-            bool result = false;
+        //bool checkInputs()
+        //{
+        //    bool result = false;
 
-            if (!string.IsNullOrEmpty(EntryName.Text) && !string.IsNullOrEmpty(EntryPassword.Text))
-                result = true;
+        //    if (!string.IsNullOrEmpty(EntryName.Text) && !string.IsNullOrEmpty(EntryPassword.Text))
+        //        result = true;
 
 
-            return result;
-        }
+        //    return result;
+        //}
 
-        async void BtnLogin(object sender, EventArgs e)
-        {
+        //async void BtnLogin(object sender, EventArgs e)
+        //{
 
-            if (checkInputs())
-            {
-                string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "UserDatabase.db");
-                var db = new SQLiteConnection(dbPath);
-                try
-                {
-                    var query = db.Table<User>().Where(u => u.UserName.Equals(EntryName.Text) && u.Password.Equals(EntryPassword.Text)).FirstOrDefault();
+        //    if (checkInputs())
+        //    {
+        //        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "UserDatabase.db");
+        //        var db = new SQLiteConnection(dbPath);
+        //        try
+        //        {
+        //            var query = db.Table<User>().Where(u => u.UserName.Equals(EntryName.Text) && u.Password.Equals(EntryPassword.Text)).FirstOrDefault();
 
-                    if (query != null)
-                    {
-                        App.Current.MainPage = new NavigationPage(new CourseCategory());
-                    }
-                    else
-                    {
-                        Device.BeginInvokeOnMainThread(async () =>
-                        {
+        //            if (query != null)
+        //            {
+        //                App.Current.MainPage = new NavigationPage(new CourseCategory());
+        //            }
+        //            else
+        //            {
+        //                Device.BeginInvokeOnMainThread(async () =>
+        //                {
 
-                            var r = await DisplayAlert("Error ", "Failed UserName and Password", "Yes", "Cancel");
-                            if (r)
-                                await Navigation.PushModalAsync(new sLoginPage());
-                            else
-                            {
-                                await Navigation.PushModalAsync(new sLoginPage());
-                            }
-                        });
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Device.BeginInvokeOnMainThread(async () =>
-                    {
-                        await DisplayAlert("Failed", "Please verify Username or Password, Or Sign Up", null, "Ok");
-                    });
+        //                    var r = await DisplayAlert("Error ", "Failed UserName and Password", "Yes", "Cancel");
+        //                    if (r)
+        //                        await Navigation.PushModalAsync(new sLoginPage());
+        //                    else
+        //                    {
+        //                        await Navigation.PushModalAsync(new sLoginPage());
+        //                    }
+        //                });
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Device.BeginInvokeOnMainThread(async () =>
+        //            {
+        //                await DisplayAlert("Failed", "Please verify Username or Password, Or Sign Up", null, "Ok");
+        //            });
 
-                }
-            }
-            else
-            {
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    await DisplayAlert("Failed", " Either Username or Password entry has not been entered ", null, "Ok");
-                });
-            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Device.BeginInvokeOnMainThread(async () =>
+        //        {
+        //            await DisplayAlert("Failed", " Either Username or Password entry has not been entered ", null, "Ok");
+        //        });
+        //    }
 
-            //await Navigation.PushModalAsync(new CourseCategory());
-        }
+        //    //await Navigation.PushModalAsync(new CourseCategory());
+        //}
 
-        async void BtnSignUp(object sender, EventArgs e)
-        {
+        //async void BtnSignUp(object sender, EventArgs e)
+        //{
 
-            await Navigation.PushModalAsync(new RegisterPage());
-        }
+        //    await Navigation.PushModalAsync(new RegisterPage());
+        //}
     }
 }
